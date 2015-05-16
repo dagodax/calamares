e2ec2007a5e3917d297a6eb95ba2b2063fc99801
demmm <anke62@gmail.com>
2015-05-16 12:19:36 -0400

    copy greeting page text to welcome page trial to fix missing var for %2 in choice page, only one var defined so far

--- src/modules/welcome/WelcomePage.cpp
+++ src/modules/welcome/WelcomePage.cpp
@@ -55,7 +55,12 @@ WelcomePage::WelcomePage( RequirementsChecker* requirementsChecker, QWidget* par
     CALAMARES_RETRANSLATE(
         ui->mainText->setText( tr( "<h1>Welcome to the %1 installer.</h1><br/>"
                                    "This program will ask you some questions and "
-                                   "set up %2 on your computer." )
+                                   "set up %2 on your computer." 
+                                   " Remember, this installer is still in an early stage.<br/>"
+                                   "Not implemented yet is LUKS/LVM or RAID and<br/>"
+                                   "using one of the automated partitioning options for UEFI installs.<br/>" 
+                                   "For <b>GPT partioning on BIOS</b> systems and "
+                                   "other instructions see the Known Issues" )
                                 .arg( Calamares::Branding::instance()->
                                       string( Calamares::Branding::VersionedName ) )
                                 .arg( Calamares::Branding::instance()->
