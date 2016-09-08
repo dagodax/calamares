5b6d8505d180dc8c3267f848a3a4c00b39ea3669
demmm <anke62@gmail.com>
2016-09-08 12:45:35 -0400

    move nonfree_drivers to nvidia 37*

--- src/modules/nonfree_drivers/main.py
+++ src/modules/nonfree_drivers/main.py
@@ -55,10 +55,10 @@ def run():
         print('installing driver')
         shutil.copytree(
             '/opt/kdeos/pkgs', '%s/opt/kdeos/pkgs' % (install_path))
-        for nvidia_utils in glob.glob('/opt/kdeos/pkgs/nvidia-utils-36*'):
+        for nvidia_utils in glob.glob('/opt/kdeos/pkgs/nvidia-utils-37*'):
             libcalamares.utils.target_env_call(
                 ['pacman', '-Ud', '--force', '--noconfirm', nvidia_utils])
-        for nvidia in glob.glob('/opt/kdeos/pkgs/nvidia-36*'):
+        for nvidia in glob.glob('/opt/kdeos/pkgs/nvidia-37*'):
             libcalamares.utils.target_env_call(
                 ['pacman', '-Ud', '--force', '--noconfirm', nvidia])
         shutil.rmtree('%s/opt/kdeos/pkgs' % (install_path))
