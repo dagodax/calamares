bfeb88f346935df7f06c9023a65f976c24952bea
demmm <anke62@gmail.com>
2018-04-18 23:48:01 +0200

    move the branding lang dir inside KaOS branding

---
+++ src/branding/kaos_branding/lang/calamares-kaos_branding_it.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="47"/>
+        <source>Here the actual install of KaOS will start.&lt;br/&gt;Use the left &lt;b&gt;mouse button&lt;/b&gt; to go to the next slide, right for previous.&lt;br/&gt;After creating your chosen disk setup in the first 10 % &lt;br/&gt;the full copying of the ISO will take the longest of this install phase &lt;br/&gt;and will run until approximately 30%.&lt;br/&gt;</source>
+        <translation>L&apos;nstallazione di KaOS sta per avere inizio. &lt;br/&gt;Premi il &lt;b&gt;pulsante del mouse&lt;/b&gt;sinistro per andare alla prossima slide, il destro per visualizzare la precedente. &lt;br/&gt; Il primo 10% servirà per attualizzare le tue scelte di partizionamento,&lt;br/&gt; per la copia completa dal supporto USB ci vorrà un po&apos; di tempo,&lt;br/&gt;fino a circa il 30% del processo di installazione.&lt;br/&gt;.  </translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="73"/>
+        <source>After the ISO is copied some 25 post-install modules will run.&lt;br/&gt;This includes setting user specific options, &lt;br/&gt;removing Live Session only packages&lt;br/&gt;and adjusting hardware setup.&lt;br/&gt;</source>
+        <translation>Dopo che la ISO verrà copiata, verranno eseguiti 25 moduli post-installazione.&lt;br/&gt;Questi includono preferenze e scelte dell&apos;utente, &lt;br/&gt;rimuovendo i pacchetti della Sessione Live&lt;br/&gt;e finiranno con la configurazione dell&apos;hardware.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="98"/>
+        <source>The default Office Suite is Calligra.&lt;br/&gt;LibreOffice is available in the repositories. &lt;br/&gt;</source>
+        <translation>Calligra è installato di default come Suite per l&apos;ufficio, &lt;br/&gt;ma LibreOffice è disponibile nella nostra selezione del software.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="121"/>
+        <source>Qt/KDE specific internet applications include the Qupzilla web-browser &lt;br/&gt;and kde-telepathy for chat and Instant Messaging. &lt;br/&gt;</source>
+        <translation>Applicazioni specifiche Qt/KDE per internet includono Qupzilla web-browser &lt;br/&gt; e kde-telepathy per chat e messaggeria istantanea. &lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="144"/>
+        <source>For package management Octopi is the GUI application.&lt;br/&gt;</source>
+        <translation>Puoi installare e rimuovere pacchetti facilmente con Octopi, l&apos;interfaccia grafica del gestore software. &lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="166"/>
+        <source>May using KaOS be a pleasant experience for you.</source>
+        <translation>Speriamo che usare KaOS sia una piacevole esperienza per te.</translation>
+    </message>
+</context>
+</TS>
---
+++ src/branding/kaos_branding/lang/calamares-kaos_branding_uk.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="uk" version="2.1">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="47"/>
+        <source>Here the actual install of KaOS will start.&lt;br/&gt;Use the left &lt;b&gt;mouse button&lt;/b&gt; to go to the next slide, right for previous.&lt;br/&gt;After creating your chosen disk setup in the first 10 % &lt;br/&gt;the full copying of the ISO will take the longest of this install phase &lt;br/&gt;and will run until approximately 30%.&lt;br/&gt;</source>
+        <translation>Тут буде запущена фактиче встановлення KaOS. &lt;br/&gt;Використовуйте ліву &lt;b&gt;кнопку миші,&lt;/b&gt; щоб перейти до наступного слйда, праву на попередній.&lt;br/&gt; Після створення обраного диска перші 10% &lt;br/&gt;займе повне копіювання ISO найдовшу з фаз встановлення &lt;br/&gt;і буде тривати приблизно до 30%.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="73"/>
+        <source>After the ISO is copied some 25 post-install modules will run.&lt;br/&gt;This includes setting user specific options, &lt;br/&gt;removing Live Session only packages&lt;br/&gt;and adjusting hardware setup.&lt;br/&gt;</source>
+        <translation>Після копіювання ISO буде запущено близько 25 модулів після встановлення. &lt;br/&gt;Це включає в себе налаштування конкретних параметрів користувача, &lt;br/&gt;видалення пакетів Live Session &lt;br/&gt;та налаштування апаратного забезпечення. &lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="98"/>
+        <source>The default Office Suite is Calligra.&lt;br/&gt;LibreOffice is available in the repositories. &lt;br/&gt;</source>
+        <translation>Стандартний офісний пакет - Calligra.&lt;br/&gt;LibreOffice доступний у сховищах. &lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="121"/>
+        <source>Qt/KDE specific internet applications include the Qupzilla web-browser &lt;br/&gt;and kde-telepathy for chat and Instant Messaging. &lt;br/&gt;</source>
+        <translation>Спеціальні інтернет-програми для Qt/KDE включають веб-браузер Qupzilla &lt;br/&gt;та kde-telepathy для чату та миттєвого обміну повідомленнями.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="144"/>
+        <source>For package management Octopi is the GUI application.&lt;br/&gt;</source>
+        <translation>Для керування пакетами Octopi - це програма з графічним інтерфейсом.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../kaos_branding/show.qml" line="166"/>
+        <source>May using KaOS be a pleasant experience for you.</source>
+        <translation>Можливо, KaOS буде для вас приємним досвідом.</translation>
+    </message>
+</context>
+</TS>
